d9ee777fcab594b2c9c79ba96572325cee2b1716
kathleenmoriarty <149009823+kathleenmoriarty@users.noreply.github.com>
2024-10-21 19:26:32 -0700

    Save data and generate HTML for delivery options

--- javascript-course/amazon-project/data/cart.js
+++ javascript-course/amazon-project/data/cart.js
@@ -1,11 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem('cart')) || [
     {
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2
+        quantity: 2,
+        deliveryOptionId: '1'
     },
     {
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '2'
     }
 
 ];
@@ -29,7 +31,8 @@ export function addToCart(productId, selectValue) {
     } else {
         cart.push({
             productId,
-            quantity: selectValue
+            quantity: selectValue,
+            deliveryOptionId: '1'
         });
     }
 
@@ -49,3 +52,22 @@ export function removeFromCart(productId) {
 
     saveToStorage();
 };
+
+export function calculateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+    return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+    cart.forEach((cartItem) => {
+        if(cartItem.id === productId) {
+            cartItem.quantity = newQuantity;
+        }
+    });
+
+    saveToStorage();
+}
